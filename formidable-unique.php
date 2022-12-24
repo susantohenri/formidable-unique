757dcc4bebaf93b9e6071acb6e7a1e5d47e17ed0
henrisusanto <henri@member.id>
2022-12-24 17:25:25 +0700

    init

---
+++ formidable-unique.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Formidable Unique
+ *
+ * @package     FormidableUnique
+ * @author      Henri Susanto
+ * @copyright   2022 Henri Susanto
+ * @license     GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name: Formidable Unique
+ * Plugin URI:  https://github.com/susantohenri
+ * Description: Formidable form add-on for generate unique increment value. example: [input data-unique="combine-field1551-and-field1550-plus-field1553"]
+ * Version:     1.0.0
+ * Author:      Henri Susanto
+ * Author URI:  https://github.com/susantohenri
+ * Text Domain: Formidable-Unique
+ * License:     GPL v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
